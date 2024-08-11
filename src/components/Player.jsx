60e5984d417024ef06d325540c0ad1f44ba5d028
DarkSJ1998 <sarthakjn12@gmail.com>
2024-08-11 19:46:06 +0530

    Added two-way data binding for the Player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const [playerName, setPlayerName] = useState('');
+	const [submitted, setSubmitted] = useState(false);
+
+	function handleChangePlayerName(event) {
+		setSubmitted(false);
+		setPlayerName(event?.target?.value);
+	}
+
+	function savePlayerName() {
+		setSubmitted(true);
+	}
+
+	return (
+		<section id='player'>
+			<h2>Welcome {submitted ? playerName : 'unknown entity'}</h2>
+			<p>
+				<input
+					type='text'
+					onChange={handleChangePlayerName}
+					value={playerName}
+				/>
+				<button onClick={savePlayerName}>Set Name</button>
+			</p>
+		</section>
+	);
 }
